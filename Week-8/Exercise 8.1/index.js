3985401e0e04cd700cf934339fe61af25eeae66e
Yashkumar Jani <89257321+yashkumarjani@users.noreply.github.com>
2023-05-27 11:29:42 +0530

    Assignment 8

---
+++ Week-8/Exercise 8.1/index.js
@@ -0,0 +1,17 @@
+// Problem 8.1 : Reverse the Linked list
+
+var reverseList = function (head) {
+    let prev = null
+    let next = null
+    while (head !== null) {
+      // saving next
+      next = head.next
+      // reverse
+      head.next = prev
+      // move prev and head
+      prev = head
+      head = next
+  
+    };
+    return prev
+  }
---
+++ Week-8/Exercise 8.3/index.js
@@ -0,0 +1,16 @@
+// Problem 8.3: Detect loop in a linked list
+
+var hasCycle = function (head) {
+  // slow and fast pointer
+    let slow = head
+    let fast = head
+  
+    while (fast && fast.next !== null) {
+      slow = slow.next
+      fast = fast.next.next
+      if(slow == fast){
+        return true
+      }
+    }
+    return false
+  };
